0bf74d9f8245e80486c36bba3af644012ba1405e
Soaring Radio <burroughszeb@me.com>
2017-03-25 22:41:58 -0500

    init

---
+++ app/src/renderer/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <h1>Hello.</h1>
+
+
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+
+    },
+    name: 'Home'
+  }
+</script>
+
+<style scoped>
+  img {
+    margin-top: -25px;
+    width: 450px;
+  }
+</style>
---
+++ app/src/renderer/routes.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: require('components/Home')
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
+]
